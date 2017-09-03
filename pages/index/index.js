048e73f5a857024d9f8f11dd7feb4bee38eb5f6f
wuyingyi <wuyingyi@99114.com>
2017-09-03 15:36:39 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,73 @@
+// pages/find/find.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    lists: [
+      { id: "001", imgurl: "../../image/1.jpg",state:1, name: '土耳其咖啡馆', msg: '[120条]宁娟：@张小炽 羡慕羡慕', time:'23:15'},
+      { id: "002", imgurl: "../../image/1.jpg", state: 2,name: '一季花开、一季花落', msg: '的思维习惯和行为习惯，从而在自相矛盾的大道理中无法前行。', time: '23:15' },
+      { id: "003", imgurl: "../../image/1.jpg", state: 1, name: '一季花开、一季花落', msg: '爱尔兰雪、土耳其蓝、莫斯科眼泪。我都收藏在小小的太阳里、还有晴天和微笑。', time: '23:15' },
+      { id: "004", imgurl: "../../image/1.jpg", state: 1, name: '一季花开、一季花落', msg: '的思维习惯和行为习惯，从而在自相矛盾的大道理中无法前行。', time:'23:15'},
+      { id: "005", imgurl: "../../image/1.jpg", state: 2, name: '一季花开、一季花落', msg: '爱你、给你我生命所有的美好、然后退场、让万花筒灿烂你的眼瞳。', time: '23:15' },
+      { id: "006", imgurl: "../../image/1.jpg", state: 1, name: '一季花开、一季花落', msg: '的思维习惯和行为习惯，从而在自相矛盾的大道理中无法前行。', time: '23:15' }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ utils/mock.js
@@ -0,0 +1,80 @@
+let discover = [
+  {
+    img: '',
+    title: '星巴克聊天群',
+    message: '21',
+    distance: '100'
+  }
+]
+
+let talk = [
+  {
+    avatar: '',
+    name: '张晓峰',
+    isMe: false,
+    mType: 'video',
+    message: '',
+    src: ''
+  },
+  {
+    avatar: '',
+    name: '',
+    isMe: false,
+    mType: 'message',
+    message: '2017年6月28日 21:10',
+    src: ''
+  },
+  {
+    avatar: '',
+    name: 'me',
+    isMe: true,
+    mType: 'text',
+    message: '这个视频给力',
+    src: ''
+  },
+  {
+    avatar: '',
+    name: '',
+    isMe: false,
+    mType: 'message',
+    message: '2017年6月28日 21:10',
+    src: ''
+  },
+  {
+    avatar: '',
+    name: '张晓峰',
+    isMe: false,
+    mType: 'img',
+    message: '',
+    src: '/image/img1.png'
+  },
+  {
+    avatar: '',
+    name: '张晓峰',
+    isMe: false,
+    mType: 'text',
+    message: '666',
+    src: ''
+  },
+  {
+    avatar: '',
+    name: '',
+    isMe: false,
+    mType: 'message',
+    message: '"Gcaufu小助手" 邀请 “志强” 加入群聊',
+    src: ''
+  },
+  {
+    avatar: '',
+    name: '',
+    isMe: false,
+    mType: 'message',
+    message: '禁止发布不良信息，被举报超过三次将会被系统自动踢出该聊天室',
+    src: ''
+  },
+]
+
+module.exports = {
+  discover,
+  talk
+}
